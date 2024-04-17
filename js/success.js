579ae788f58a5b1f9bc93680305a5ba9e1aec811
Iwo Paprota <iwo.paprota@gmail.com>
2024-04-17 23:56:31 +0200

    Copy to clipboard snackbar

--- js/success.js
+++ js/success.js
@@ -17,12 +17,26 @@ window.addEventListener('DOMContentLoaded', event => {
   const receiver = document.getElementById('receiver');
   const accountNumber = document.getElementById('account-number');
   const transferTitle = document.getElementById('transfer-title');
+    const snackbar = document.getElementById('snackbar');
+
+  function showToast() {
+    snackbar.style.display = 'block';
+    setTimeout(function () {
+      snackbar.style.display = 'none';
+    }, 1000);
+    snackbar.className = "show";
+
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function(){ snackbar.className = snackbar.className.replace("show", ""); }, 3000);
+  }
 
   receiver.addEventListener('click', function() {
     this.select();
     this.setSelectionRange(0, 99999);
     document.execCommand('copy');
     window.getSelection().removeAllRanges();
+
+    showToast();
   });
 
   accountNumber.addEventListener('click', function() {
@@ -30,6 +44,8 @@ window.addEventListener('DOMContentLoaded', event => {
     this.setSelectionRange(0, 99999);
     document.execCommand('copy');
     window.getSelection().removeAllRanges();
+
+    showToast();
   });
 
   transferTitle.addEventListener('click', function() {
@@ -37,6 +53,10 @@ window.addEventListener('DOMContentLoaded', event => {
     this.setSelectionRange(0, 99999);
     document.execCommand('copy');
     window.getSelection().removeAllRanges();
+
+    showToast();
   });
+
+
 
 });
