6807d019d1e9f31cc139087a5126acaeae90ed5b
Iwo Paprota <iwo.paprota@gmail.com>
2024-04-17 23:30:45 +0200

    Layout && copy to clipboard

--- js/success.js
+++ js/success.js
@@ -11,6 +11,32 @@ window.addEventListener('DOMContentLoaded', event => {
   const price = getQueryParam('price');
 
   // Set the values as placeholders for the input fields
-  document.getElementById('transfer-title').placeholder = title || 'Title';
-  document.getElementById('amount').placeholder = price + ' PLN' || 'Price';
+  document.getElementById('transfer-title').value = title || 'Title';
+  document.getElementById('amount').value = (price || '0') + ' PLN' || 'Price';
+
+  const receiver = document.getElementById('receiver');
+  const accountNumber = document.getElementById('account-number');
+  const transferTitle = document.getElementById('transfer-title');
+
+  receiver.addEventListener('click', function() {
+    this.select();
+    this.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+  });
+
+  accountNumber.addEventListener('click', function() {
+    this.select();
+    this.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+  });
+
+  transferTitle.addEventListener('click', function() {
+    this.select();
+    this.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+  });
+
 });
